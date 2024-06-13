255ec207c5bf70acd700b039ce36410ed3eba88c
Phong Mai <113613135+Phong-Mai@users.noreply.github.com>
2024-06-13 20:21:58 +0700

    56456

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://story-sever.vercel.app',
         secure: true,
       },
     },
