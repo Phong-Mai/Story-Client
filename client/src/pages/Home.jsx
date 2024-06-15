6b15df1a4e3640eec72cc23cb08073c3e971e19e
Phong Mai <113613135+Phong-Mai@users.noreply.github.com>
2024-06-15 19:30:05 +0700

    hihi

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   // console.log(process.env.SEVER);
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await fetch(`https://story-sever.vercel.app/api/post/getPosts`);
+      const res = await fetch(`/api/post/getPosts`);
       const data = await res.json();
       setPosts(data.posts);
     };
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   server: {
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:3000',
+    //     secure: true,
+    //   },
+    // },
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: true,
-      },
+      '/api': 'https://story-sever.vercel.app',
     },
   },  
+ 
   plugins: [react()],
 });
